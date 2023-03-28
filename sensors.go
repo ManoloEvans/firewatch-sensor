f161250b0b5b0ab0a8d84fe61a1b6f0b15d21d1f
Manolo Rosario Evans Diaz <dragon.evans@gmail.com>
2023-03-28 22:00:24 +0100

    sensors.go: added sensors.go contains functions to start sensor and read co2,humidity, temperature

---
+++ sensors.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"machine"
+
+	"tinygo.org/x/drivers/scd4x"
+)
+
+var (
+	// devices
+	co2Sensor *scd4x.Device
+
+	// readings
+	co2SensorReading int32
+	temperature      int32
+	humidity         int32
+)
+
+func startSensors() {
+	machine.I2C0.Configure(machine.I2CConfig{})
+
+	co2Sensor = scd4x.New(machine.I2C0)
+	co2Sensor.Configure()
+
+}
+
+func readSensors() {
+	temperature, _ = co2Sensor.ReadTemperature()
+	co2SensorReading, _ = co2Sensor.ReadCO2()
+	humidity, _ = co2Sensor.ReadHumidity()
+
+	println("Temperature", temperature, "°C")
+	println("Humidity", humidity, "%")
+	println("CO2", co2SensorReading, "ppm")
+
+}
